ed05f36c682d4f6000bc0c495993dbfab5e0f600
Alexandr Kozlenkov <sah4ez32@gmail.com>
2018-02-27 21:31:02 +0300

    fixed test

--- internal/dependency/glide/glide_test.go
+++ internal/dependency/glide/glide_test.go
@@ -1,14 +1,22 @@
 package glide
 
 import (
-	"os"
 	"testing"
 
+	"github.com/sah4ez/gosvm/internal/dependency"
 	"github.com/sah4ez/gosvm/internal/structure"
 )
 
 func TestLoadGlide(t *testing.T) {
-	pwd, err := os.Getwd()
+	//exp := make(map[string]map[string][]string)
+
+	//exp := {
+	//	"import/first/package":{
+	//		"1.0.0":{"sub_project1", "sub_project2"},
+	//	},
+	//	"import/second/package":{
+	//		"2.0.0":{"sub_project1", "sub_project2"},
+	//	}, //} exp := {""} pwd, err := os.Getwd()
 	if err != nil {
 		t.Error("Could not get pwd")
 	}
@@ -16,10 +24,10 @@ func TestLoadGlide(t *testing.T) {
 	if err != nil {
 		t.Fatalf("Couldn't load config. Error %s", err)
 	}
+	packs := dependency.NewPackages()
+	gl := NewGlideLoader(root, packs)
 
-	gl := NewGlideLoader(root)
-
-	packs, err := gl.Load()
+	packs, err = gl.Load()
 	if err != nil {
 		t.Error("could not load libs map. ", err)
 	}
