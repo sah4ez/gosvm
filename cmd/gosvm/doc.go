502d51414d87a73c1053825166fa01993cea6213
Alexandr Kozlenkov <sah4ez32@gmail.com>
2018-05-25 10:05:57 +0300

    Added doc command.

--- cmd/gosvm/doc.go
+++ cmd/gosvm/doc.go
@@ -4,6 +4,9 @@ import (
 	"io/ioutil"
 	"os"
 
+	stdfmt "fmt"
+
+	toml "github.com/pelletier/go-toml"
 	"github.com/sah4ez/gosvm/fs"
 	fmt "github.com/sah4ez/gosvm/pkg/formatting"
 	"github.com/sah4ez/gosvm/pkg/structure"
@@ -27,6 +30,15 @@ func (l *docCmd) Run(args []string) error {
 
 	switch len(args) {
 	case 1:
+		os.Remove(wd + "/svm_doc.toml")
+		if err != nil {
+			return err
+		}
+		docs, err := os.OpenFile(wd+"/svm_doc.toml", os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0644)
+		if err != nil {
+			return err
+		}
+		defer docs.Close()
 		root, err := structure.LoadStructure(wd + "/svm.toml")
 		if err != nil {
 			return err
@@ -34,22 +46,47 @@ func (l *docCmd) Run(args []string) error {
 
 		for _, sub := range root.SubProject {
 			fullPath := fs.PathToProject(root.BasePath, sub.Title)
+			if !fs.ExistsGoProject(root.BasePath, sub.Title) {
+				return ErrProjectNotFound
+			}
 			specFile := fullPath + "/svm.toml"
 			if !fs.Exists(specFile) {
 				fmt.Warn.Fprintln(l.w, "[WARN] file not exist: "+specFile)
+				b, err := toml.Marshal(sub)
+				if err != nil {
+					return nil
+				}
+				err = writeSubProject(docs, b)
+				if err != nil {
+					return err
+				}
 				continue
 			}
 			data, err := ioutil.ReadFile(specFile)
 			if err != nil {
 				return err
 			}
-			if !fs.ExistsGoProject(root.BasePath, sub.Title) {
-				return ErrProjectNotFound
-			}
-			fmt.Info.Fprintln(l.w, "")
-			fmt.Info.Fprintln(l.w, string(data))
-			fmt.Info.Fprintln(l.w, "")
+			writeSubProject(docs, data)
+			stdfmt.Fprintln(l.w, specFile)
+			stdfmt.Fprintln(l.w, string(data))
+			stdfmt.Fprintln(l.w, "")
 		}
+	}
+	return nil
+}
+
+func writeSubProject(file *os.File, data []byte) error {
+	_, err := file.WriteString("[SubPackage]\n")
+	if err != nil {
+		return err
+	}
+	_, err = file.Write(data)
+	if err != nil {
+		return err
+	}
+	_, err = file.WriteString("\n")
+	if err != nil {
+		return err
 	}
 	return nil
 }
